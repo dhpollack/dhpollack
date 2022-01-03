3ab63e82edcc9827499c76a0e2c7030f880d3287
David Pollack <david@da3.net>
2022-01-03 21:44:57 +0100

    Add cover letter

--- resume_v2/generate-pdfs.sh
+++ resume_v2/generate-pdfs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 zola build
-sed -i 's/https:&#x2F;&#x2F;dhpollack.github.io&#x2F;dhpollack&#x2F;//g' public/index.html
+sed -i 's/https:&#x2F;&#x2F;dhpollack.github.io&#x2F;dhpollack&#x2F;//g' public/index.html public/coverletter-with-frontmatter/index.html
 chromium --headless --disable-gpu --no-margins --print-to-pdf-no-header --print-to-pdf=static/david-pollack-resume.pdf public/index.html
+chromium --headless --disable-gpu --no-margins --print-to-pdf-no-header --print-to-pdf=static/david-pollack-cover-letter.pdf public/coverletter-with-frontmatter/index.html
